377116b576ed06e9d7cf8b991875fb97b2ae4ceb
adriente <adrien.teurtrie@cemes.fr>
2026-07-16 18:45:07 +0200

    multi electron states

--- src/electron_state.py
+++ src/electron_state.py
@@ -1,3 +1,7 @@
+r"""
+Core class and functions of pyiels. 
+"""
+
 from qutip import Qobj, tensor
 import numpy as np
 import math
@@ -10,7 +14,7 @@ from itertools import pairwise
 __all__ = ['ElectronState']
 
 class ElectronState :
-    def __init__(self,electron_state : Qobj, data : Qobj = None, dim_indices : list[int] = None) :
+    def __init__(self,electron_state : Qobj, data : Qobj = None, dim_indices : list[list[int]] = None) :
         self.electron_state = electron_state
         if data is None :
             self._qobj = electron_state.copy()
@@ -18,7 +22,7 @@ class ElectronState :
             self._qobj = data.copy()
         self.es_dims = self.electron_state.dims
         if dim_indices is None :
-            self.es_dims_indices = [0 for i in range(len(self.es_dims))]
+            self.es_dims_indices = [[0 for i in range(len(self.es_dims))]]
         else :
             self.es_dims_indices = dim_indices
         self.es_indices = self.get_es_indices()
@@ -28,18 +32,20 @@ class ElectronState :
         Fetch the electron state indices from the total q object.
         """
         es_indices = []
-
         dim_list = self._qobj.dims
-         
-        es_ind = self.es_dims_indices[0]
-        shape_left = math.prod(dim_list[0][:es_ind])
-        shape_right = math.prod(dim_list[0][es_ind+1:])
-        es_indices.append([x for x in range(dim_list[0][es_ind]) for _ in range(shape_right)]*shape_left)
+        for dims in self.es_dims_indices : 
+            single_es_indices = []
+            es_ind = dims[0]
+            shape_left = math.prod(dim_list[0][:es_ind])
+            shape_right = math.prod(dim_list[0][es_ind+1:])
+            single_es_indices.append([x for x in range(dim_list[0][es_ind]) for _ in range(shape_right)]*shape_left)
 
-        es_ind = self.es_dims_indices[1]
-        shape_left = math.prod(dim_list[1][:es_ind])
-        shape_right = math.prod(dim_list[1][es_ind+1:])
-        es_indices.append([x for x in range(dim_list[1][es_ind]) for _ in range(shape_right)]*shape_left)
+            es_ind = dims[1]
+            shape_left = math.prod(dim_list[1][:es_ind])
+            shape_right = math.prod(dim_list[1][es_ind+1:])
+            single_es_indices.append([x for x in range(dim_list[1][es_ind]) for _ in range(shape_right)]*shape_left)
+
+            es_indices.append(single_es_indices)
         
         return es_indices
         # es_indices = []
@@ -232,17 +238,19 @@ shape = [4, 4], type = oper, isHerm = True
     if len(es_positions) == 0 : 
         raise ValueError("None of the arguments are Electron states. The etensor applies on ElectronState, use qutip tensor for qutip only usage.")
     
-    if len(es_positions) > 1 : 
-        raise NotImplementedError("We can't handle tensor of multiple electron states.")
+    # if len(es_positions) > 1 : 
+    #     raise NotImplementedError("We can't handle tensor of multiple electron states.")
 
+    dim_indices = []
+    elec_state = []
     isherm = args[0]._isherm
     isunitary = args[0]._isunitary
     out_data = args[0].data
     dims_l = [args[0]._dims[0]]
     dims_r = [args[0]._dims[1]]
     if isinstance(args[0],ElectronState) :
-        dim_indices = [0,0]
-        elec_state = args[0].electron_state
+        dim_indices.append([0,0])
+        elec_state.append(args[0].electron_state)
 
     for i, arg in enumerate(args[1:]):
         out_data = _data.kron(out_data, arg.data)
@@ -253,8 +261,8 @@ shape = [4, 4], type = oper, isHerm = True
         dims_l.append(arg._dims[0])
         dims_r.append(arg._dims[1])
         if isinstance(arg,ElectronState) :
-            dim_indices = [i+1,i+1]
-            elec_state = arg.electron_state
+            dim_indices.append([i+1,i+1])
+            elec_state.append(arg.electron_state)
 
     qobj = Qobj(out_data,
                 dims=[dims_l,dims_r],
@@ -262,12 +270,16 @@ shape = [4, 4], type = oper, isHerm = True
                 isunitary=isunitary,
                 copy = False)
     
-    if qobj.dims[0] == [1] : 
-        dim_indices[0] = 0
-    if qobj.dims[1] == [1] : 
-        dim_indices[1] = 0
+    final_elec_state = tensor(elec_state)
+    
+    if qobj.dims[0] == [1] :
+        for dim in dim_indices :
+            dim[0] = 0
+    if qobj.dims[1] == [1] :
+        for dim in dim_indices :
+            dim[1] = 0
 
-    return ElectronState(elec_state,
+    return ElectronState(final_elec_state,
                 qobj,
                 dim_indices=dim_indices)
 
@@ -281,53 +293,3 @@ if __name__ == '__main__' :
     # es._qobj[*idx]
     a = basis(5,1) * basis(2,1).dag()
     es * a
-
-from functools import wraps
-
-def vectorize_operation(method):
-    """
-    Decorator to make a method handle both single custom objects and tuples of them.
-    """
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        # We assume the first argument (or self) is our wrapper object.
-        # If the method is binary (e.g., self + other), we check 'other'.
-        
-        # Scenario 1: No extra positional arguments (unary operation, e.g., self.dagger())
-        if not args:
-            # If the wrapper object itself holds a tuple internally
-            if isinstance(self.data, tuple):
-                # Apply the method to a temporary instance created for each element
-                return tuple(method(self.__class__(item), **kwargs) for item in self.data)
-            return method(self, **kwargs)
-
-        # Scenario 2: Binary operations (e.g., self + other)
-        other = args[0]
-        
-        # Case A: 'other' is a tuple
-        if isinstance(other, tuple):
-            # If self is also representing a tuple internally, pair them up (zip)
-            if isinstance(self.data, tuple):
-                if len(self.data) != len(other):
-                    raise ValueError("Tuple lengths must match for mathematical operations.")
-                return tuple(
-                    method(self.__class__(s), self._unwrap(o), *args[1:], **kwargs)
-                    for s, o in zip(self.data, other)
-                )
-            # If self is single, distribute self across the tuple of 'other'
-            return tuple(
-                method(self, self._unwrap(o), *args[1:], **kwargs) 
-                for o in other
-            )
-            
-        # Case B: 'other' is single, but 'self' holds a tuple internally
-        if isinstance(self.data, tuple):
-            return tuple(
-                method(self.__class__(s), self._unwrap(other), *args[1:], **kwargs)
-                for s in self.data
-            )
-
-        # Case C: Both are single objects
-        return method(self, *args, **kwargs)
-
-    return wrapper
